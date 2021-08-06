bd666dab230cacc602344f5ca595f78a0a1290ea
S. M. Sabiul Hajjaj <39924795+SabiulSabit@users.noreply.github.com>
2021-08-06 21:50:20 +0600

    Add files via upload

---
+++ Day 08/types.ts
@@ -0,0 +1,14 @@
+//date type variable
+const today = new Date();
+
+//custome object
+const person = {
+    age: 20
+}
+
+//custome class
+class Color {
+    
+}
+//hold the color class referance
+const res =  new Color();
