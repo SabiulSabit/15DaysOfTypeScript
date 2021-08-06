62175734409417f0a10fda0a88c39cc34a75a486
S. M. Sabiul Hajjaj <39924795+SabiulSabit@users.noreply.github.com>
2021-08-06 22:07:31 +0600

    Add files via upload

---
+++ Day 08/annotations/variables.ts
@@ -0,0 +1,33 @@
+ //declear a variable with type anotations
+ let apples: number = 5;
+ let username: string = "Sabit";
+ let admin: boolean = true;
+ let empty: null = null;
+ let nothing: undefined = undefined;
+ let today: Date = new Date();
+
+ //array
+ let arr_string: string[] = ["a", "b", "c"];
+ let arr_number: number[] = [1, 2, 3];
+
+
+ //class
+ class Car{
+
+ }
+ let car: Car = new Car();
+
+ //object literal
+ let point:  {x: number; y: number} = {
+     x: 10,
+     y: 20
+ }
+
+
+ //function
+ 
+
+ //valid and invalid way to access or modify those variable
+ apples = 10;           // valid
+ //apples =  "Sabit";   // invalid 
+
