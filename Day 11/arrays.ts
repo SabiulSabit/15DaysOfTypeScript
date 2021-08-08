1d5182acf4a359d14daec59bd2b54b0f7073ece0
S. M. Sabiul Hajjaj <39924795+SabiulSabit@users.noreply.github.com>
2021-08-08 21:49:46 +0600

    Add files via upload

---
+++ Day 11/arrays.ts
@@ -0,0 +1,14 @@
+//array of string only
+const carMakers = ["ford", "toyota", "chevy"]
+
+const carName: string[] = []
+
+//any type array
+const arr = [];
+
+//valid
+carMakers.push('lambo')
+
+//invalid
+//carMakers.push(233);
+
