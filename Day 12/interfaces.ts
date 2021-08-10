ba210da95ac7470a9adcedfd0ba5c1d936c1fa53
S. M. Sabiul Hajjaj <39924795+SabiulSabit@users.noreply.github.com>
2021-08-10 23:18:55 +0600

    Update interfaces.ts

--- Day 12/interfaces.ts
+++ Day 12/interfaces.ts
@@ -1,10 +1,18 @@
+//create an interface
+interface Vehicle {
+    name: string;
+    year: number;
+    broken: boolean;
+}
+
 const oldCivic = {
     name: 'civic',
     year: 2000,
     broken: true
 }
 
-const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
+//use interface
+const printVehicle = (vehicle: Vehicle): void => {
     console.log(`Name: ${vehicle.name}`);
     console.log(`Name: ${vehicle.year}`);
     console.log(`Name: ${vehicle.broken}`);
