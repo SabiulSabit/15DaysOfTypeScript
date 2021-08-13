43efcc2db4eb07243876379c8798c5dbdc4f9e27
S. M. Sabiul Hajjaj <39924795+SabiulSabit@users.noreply.github.com>
2021-08-13 10:59:25 +0600

    Update classes.ts

--- Day 13/classes.ts
+++ Day 13/classes.ts
@@ -6,8 +6,25 @@ class Vehicle {
     }
 }
 
+
+//inheritance
+class Car extends Vehicle {
+
+    //override
+    drive(): void {
+        console.log("Hello from Car Drive");
+
+    }
+}
+
+
 //class instance create
 const vehicle = new Vehicle();
 
 //use class propery
 vehicle.drive();
+
+//use car class
+const abc = new Car();
+abc.drive();
+
