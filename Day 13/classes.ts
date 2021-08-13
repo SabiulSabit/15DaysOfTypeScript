243ff598ff481cd9a2552d9f8b58b8e1f6871ab1
S. M. Sabiul Hajjaj <39924795+SabiulSabit@users.noreply.github.com>
2021-08-13 10:54:17 +0600

    Add files via upload

---
+++ Day 13/classes.ts
@@ -0,0 +1,13 @@
+//create a class
+class Vehicle {
+    drive(): void {
+        console.log("Hello from Drive");
+
+    }
+}
+
+//class instance create
+const vehicle = new Vehicle();
+
+//use class propery
+vehicle.drive();
